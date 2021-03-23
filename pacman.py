968888e44615602449d45678003bd7003bf76231
Marianna <marianna.jucewicz@gmail.com>
2021-03-23 14:56:23 +0100

    Naprawione od review, usuniete .idea/

--- pacman.py
+++ pacman.py
@@ -1,22 +1,23 @@
 import pygame
 import sys
 from pygame.locals import *
-Pacman=[]
-Pacman.append(pygame.image.load('img/pacman1.png'))
-Pacman.append(pygame.image.load('img/pacman2.png'))
-Pacman.append(pygame.image.load('img/pacman3.png'))
+Pacman = [pygame.image.load('img/pacman1.png'),
+          pygame.image.load('img/pacman2.png'),
+          pygame.image.load('img/pacman3.png'),
+          ]
 
-def drawPacman(display,x,y,i):
+clock = pygame.time.Clock()
+HEIGHT = 450
+WIDTH = 400
+BLACK = (0, 0, 0)
+
+
+def draw_pacman(display, x, y, i):
     display.blit(Pacman[i//9], (x,y))
+
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    HEIGHT = 450
-    WIDTH = 400
-    BLACK=(0,0,0)
-
-
     display = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Pacman")
     animate = 0
@@ -26,12 +27,12 @@ def main():
             pygame.quit()
             sys.exit(0)
         display.fill(BLACK)
-        drawPacman(display,WIDTH/2-32,HEIGHT/2-32,animate)
+        draw_pacman(display, WIDTH/2-32, HEIGHT/2-32, animate)
         clock.tick(27)
         pygame.display.flip()
-        animate+=1
-        if animate>=27:
-            animate=0
+        animate += 1
+        if animate >= 27:
+            animate = 0
         pygame.display.update()
 
 
