5eb726bed07090bd0080e9a42ba0bb3c4a3c79c8
Bartosz Juraszewski <shadow38pl@gmail.com>
2021-04-02 01:49:49 +0200

    Import wszystkich klas dla pewności

--- src/import_test.py
+++ src/import_test.py
@@ -1,9 +1,25 @@
 from actor import Actor
 from controller import Controller
+from direction import Direction
 from entity import Entity
+from entity_dictionary import EntityDictionary
+from model import Model
+from position import Position
+from renderer import Renderer
+from texture import Texture
 from world import World
+from world_position import WorldPosition
 
-
-world: World = World((20, 20));
+actor: Actor = None
+controller: Controller = None
+direction: Direction = Direction.UP
+entity: Entity = None
+entityEntry: EntityDictionary = None
+texture: Texture = Texture.PACMAN_0
+position: Position = Position(0.5, 0.5)
+model: Model = Model(direction, texture, position, position)
+renderer: Renderer = Renderer(None)
+world: World = World((20, 20))
+worldPosition: WorldPosition = WorldPosition(1, 1)
 
 print(world.grid)
