d1589a7092b3651b886bd92e5054a5c412d18228
Bartosz Juraszewski <shadow38pl@gmail.com>
2021-04-08 22:43:06 +0200

    Player controller i movement playera

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import sys
 
 from direction import Direction
 from player import Player
-from random_controller import RandomController
+from player_controller import PlayerController
 from renderer import Renderer
 from vector2 import Vector2Int
 from wall import Wall
@@ -22,7 +22,8 @@ def main():
     renderer = Renderer(display)
     world = World(Vector2Int(20, 20))
 
-    player = Player(RandomController(), Direction.UP)
+    playerController = PlayerController()
+    player = Player(playerController, Direction.UP)
 
     world.putEntity(Wall(), Vector2Int(5, 7))
     world.putEntity(Wall(), Vector2Int(5, 8))
@@ -34,14 +35,21 @@ def main():
     world.putEntity(Wall(), Vector2Int(5, 11))
     world.putActor(player, Vector2Int(10, 10))
 
+    keys = []
     while True:
-        event = pygame.event.poll()
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit(0)
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+            elif event.type == pygame.KEYDOWN:
+                keys.append(event.key)
+            elif event.type == pygame.KEYUP:
+                keys.remove(event.key)
 
         deltaTime = clock.tick(60) / 1000.0
 
+        playerController.updateKeys(keys)
         world.update(deltaTime)
 
         renderer.render(world)
